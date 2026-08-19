01aea7d60bbf69432798433f4e72e4b3d0431a73
benita-A11y <2578454550@qq.com>
2026-08-19 16:37:20 +0800

    update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* 热量手账 Service Worker：离线优先，网络优先更新 */
-const CACHE = 'reliang-v4';
+const CACHE = 'reliang-v5';
 const ASSETS = [
   './',
   './index.html',
@@ -8,6 +8,7 @@ const ASSETS = [
   './js/store.js',
   './js/data.js',
   './js/ai.js',
+  './js/llm.js',
   './js/app.js',
   './js/pages1.js',
   './js/pages2.js',
